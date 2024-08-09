a36a8d908ade81c63872a4dd0680eff8720e6e32
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-08-09 16:23:16 +0545

    project-initialization

---
+++ 02_passHash/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const path = require("path");
+
+const PORT = process.env.PORT;
+const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
